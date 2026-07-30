6634011e04008d1c2f2bba2f4653d65db7598891
Harman Bhogal <bhogalharman7@gmail.com>
2026-07-29 21:53:35 -0700

    imported matplot lib. Will begin plotting the values

--- percepCHP1/irisExample.py
+++ percepCHP1/irisExample.py
@@ -1,10 +1,13 @@
-import os
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 from perceptron import perceptron
 
-link = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.-data"
+link = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 print("From URL;", link)
 dataFrame = pd.read_csv(link, header=None, encoding="utf-8")
 
-dataFrame.tail()
+print(dataFrame.tail())
+
+y = dataFrame.iloc
