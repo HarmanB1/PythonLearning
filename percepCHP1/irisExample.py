f25d3d5904383d6f01aa9701cf3086a4842ba8c7
Harman Bhogal <bhogalharman7@gmail.com>
2026-08-01 19:22:28 -0700

    tested percep model on iris example

--- percepCHP1/irisExample.py
+++ percepCHP1/irisExample.py
@@ -10,4 +10,14 @@ dataFrame = pd.read_csv(link, header=None, encoding="utf-8")
 
 print(dataFrame.tail())
 
-y = dataFrame.iloc
+y = dataFrame.iloc[0:100, 4].values  # values converts to numpy arr
+y = np.where(y == "Iris-setosa", 0, 1)
+X = dataFrame.iloc[0:100, [0, 2]].values  # gets first and 3rd cols
+
+plt.scatter(X[:50, 0], X[:50, 1], color="red", marker="o", label="Setosa")
+plt.scatter(X[50:100, 0], X[50:100, 1], color="blue", marker="s")
+
+
+ppn = perceptron()
+ppn.fit(X, y)
+print(ppn.predict(X))
--- percepCHP1/perceptron.py
+++ percepCHP1/perceptron.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class perceptron:
-    def __init_(self, eta=0.01, nIter=50, randomState=1):
+    def __init__(self, eta=0.01, nIter=50, randomState=1):
         self.eta = eta
         self.nIter = nIter
         self.randomState = randomState
@@ -19,15 +19,15 @@ class perceptron:
             errors = 0
             for xi, target in zip(X, y):  # pairs exmaple with real out
                 predictY = np.dot(xi, self.w_) + self.b_
-                deltaW = self.eta * (y - predictY) * xi
-                deltab = self.eta * (y - predictY)
+                deltaW = self.eta * (target - predictY) * xi
+                deltab = self.eta * (target - predictY)
 
                 self.w_ += deltaW
                 self.b_ += deltab
 
-                errors += int((y - predictY) != 0.0)
+                errors += int((target - predictY) != 0.0)
                 self.errors_.append(errors)
-            return self
+        return self
 
     def predict(self, X):
         return np.where((np.dot(X, self.w_) + self.b_) >= 0.0, 1, 0)
