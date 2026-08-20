d29e3e5adbe544bb0bf2042d0df6ce58d570bb11
Harman Bhogal <bhogalharman7@gmail.com>
2026-08-19 21:24:05 -0700

    using sklearn to use perceptron model

---
+++ chp3/percepSkit.py
@@ -0,0 +1,14 @@
+from sklearn import datasets
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+print("Class labels:", np.unique(y))
+
+X_train, X_test, y_train, y_test = train_test_split(
+    test_size=0.3, random_state=1, stratify=y
+)
+# stratify makes sure porpotions are correct in test and train
